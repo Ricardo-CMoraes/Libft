f40ebebc517913da137cc524ac1de601fec1d0f3
Ricardo Moraes <ric.costamoraes@gmail.com>
2025-07-23 01:34:02 -0300

    Ft_putchar_fd

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdcm <rdcm@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/23 01:23:44 by rdcm              #+#    #+#             */
-/*   Updated: 2025/07/23 01:23:46 by rdcm             ###   ########.fr       */
+/*   Created: 2025/07/23 01:23:57 by rdcm              #+#    #+#             */
+/*   Updated: 2025/07/23 01:29:17 by rdcm             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char	*s, void (*f)(unsigned int, char*))
 {
-	char		*new_string;
-	unsigned	i;
+	unsigned int	i;
 
 	if (!s || !f)
-		return (0);
-	new_string = malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if(!new_string)
-		return (0);
+		return;
 	i = 0;
 	while (s[i] != '\0')
 	{
-		new_string[i] = f(i, s[i]);
+		f(i, &s[i]);
 		i++;
 	}
-	new_string[i] = '\0';
-	return (new_string);
 }
